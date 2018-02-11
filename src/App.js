d7f5b12b9e67bac042a0670c97be9a7f667f4976
lenaryan <35894595+lenaryan@users.noreply.github.com>
2018-02-11 21:20:09 +0300

    added fonts, smooth scroll and changed the way the menu opens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Home from './Pages/Home';
 import './Styles/CSS/default.min.css';
+import './scripts.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App"> 
-        <Home/>      
+        <Home/>
       </div>
     );
   }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+    rotateHamburger() {
+        document.getElementById('toggle-label').classList.toggle('rotate');
+        for (let i=0; i<document.getElementById('nav').children.length;i++)
+        document.getElementById('nav').children[i].classList.toggle('appear');
+    }
+
   render() {
     return (
       <div className="home">
@@ -13,31 +19,30 @@ class Home extends Component {
                 <span style={{color: '#f98ff7'}}> dev</span> &amp; 
                 <span style={{color: '#a946cc'}}> musician</span></p>
             </div>
-        </div>
-        <input type="checkbox" id="check-toggle"/>  
-        <label for="check-toggle" id="toggle-label"><span id="toggle"></span></label>
+        </div> 
+        <div id="toggle-label" onClick={this.rotateHamburger.bind(this)}><span id="toggle"></span></div>
         <ul id="nav">
-            <li><a href="#"><span>Works</span></a></li>
-            <li><a href="#"><span>Arts</span></a></li>
-            <li><a href="#"><span>Blog</span></a></li>
-            <li><a href="#"><span>It's Me</span></a></li>
+            <li><a href="#works"><span>Works</span></a></li>
+            <li><a href="#arts"><span>Arts</span></a></li>
+            <li><a href="#blog"><span>Blog</span></a></li>
+            <li><a href="#me"><span>It's Me</span></a></li>
         </ul>
-        <div class="wrapper">
-            <div class="block-info">
-                <div class="color" id="works">Works</div>
-                <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quam. Itaque, obcaecati maxime tempora doloremque sequi, natus dolorem perferendis illo nostrum unde cupiditate rerum doloribus dicta repellendus ipsa odio repudiandae nisi fuga vitae tenetur? Enim molestiae fugit, quisquam voluptas iure possimus odit eligendi excepturi placeat rem aperiam dolorem voluptate dignissimos doloremque fuga amet vero quasi accusamus libero consectetur quis. Commodi facere obcaecati est soluta ad totam illo provident itaque perspiciatis eaque explicabo laboriosam, excepturi odit quia, ex, necessitatibus facilis delectus ipsum voluptatem voluptatibus assumenda quidem molestiae. Velit doloribus optio doloremque id est ex eos earum distinctio! Illum mollitia quam vitae.</div>
+        <div className="wrapper">
+            <div className="block-info">
+                <div className="color" id="works">Works</div>
+                <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quam. Itaque, obcaecati maxime tempora doloremque sequi, natus dolorem perferendis illo nostrum unde cupiditate rerum doloribus dicta repellendus ipsa odio repudiandae nisi fuga vitae tenetur? Enim molestiae fugit, quisquam voluptas iure possimus odit eligendi excepturi placeat rem aperiam dolorem voluptate dignissimos doloremque fuga amet vero quasi accusamus libero consectetur quis. Commodi facere obcaecati est soluta ad totam illo provident itaque perspiciatis eaque explicabo laboriosam, excepturi odit quia, ex, necessitatibus facilis delectus ipsum voluptatem voluptatibus assumenda quidem molestiae. Velit doloribus optio doloremque id est ex eos earum distinctio! Illum mollitia quam vitae.</div>
             </div>
-            <div class="block-info">
-                    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quam. Itaque, obcaecati maxime tempora doloremque sequi, natus dolorem perferendis illo nostrum unde cupiditate rerum doloribus dicta repellendus ipsa odio repudiandae nisi fuga vitae tenetur? Enim molestiae fugit, quisquam voluptas iure possimus odit eligendi excepturi placeat rem aperiam dolorem voluptate dignissimos doloremque fuga amet vero quasi accusamus libero consectetur quis. Commodi facere obcaecati est soluta ad totam illo provident itaque perspiciatis eaque explicabo laboriosam, excepturi odit quia, ex, necessitatibus facilis delectus ipsum voluptatem voluptatibus assumenda quidem molestiae. Velit doloribus optio doloremque id est ex eos earum distinctio! Illum mollitia quam vitae.</div>
-                <div class="color" id="arts">Arts</div>
+            <div className="block-info">
+                    <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quam. Itaque, obcaecati maxime tempora doloremque sequi, natus dolorem perferendis illo nostrum unde cupiditate rerum doloribus dicta repellendus ipsa odio repudiandae nisi fuga vitae tenetur? Enim molestiae fugit, quisquam voluptas iure possimus odit eligendi excepturi placeat rem aperiam dolorem voluptate dignissimos doloremque fuga amet vero quasi accusamus libero consectetur quis. Commodi facere obcaecati est soluta ad totam illo provident itaque perspiciatis eaque explicabo laboriosam, excepturi odit quia, ex, necessitatibus facilis delectus ipsum voluptatem voluptatibus assumenda quidem molestiae. Velit doloribus optio doloremque id est ex eos earum distinctio! Illum mollitia quam vitae.</div>
+                <div className="color" id="arts">Arts</div>
             </div>
-            <div class="block-info">
-                <div class="color" id="blog">Blog</div>
-                <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quam. Itaque, obcaecati maxime tempora doloremque sequi, natus dolorem perferendis illo nostrum unde cupiditate rerum doloribus dicta repellendus ipsa odio repudiandae nisi fuga vitae tenetur? Enim molestiae fugit, quisquam voluptas iure possimus odit eligendi excepturi placeat rem aperiam dolorem voluptate dignissimos doloremque fuga amet vero quasi accusamus libero consectetur quis. Commodi facere obcaecati est soluta ad totam illo provident itaque perspiciatis eaque explicabo laboriosam, excepturi odit quia, ex, necessitatibus facilis delectus ipsum voluptatem voluptatibus assumenda quidem molestiae. Velit doloribus optio doloremque id est ex eos earum distinctio! Illum mollitia quam vitae.</div>
+            <div className="block-info">
+                <div className="color" id="blog">Blog</div>
+                <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quam. Itaque, obcaecati maxime tempora doloremque sequi, natus dolorem perferendis illo nostrum unde cupiditate rerum doloribus dicta repellendus ipsa odio repudiandae nisi fuga vitae tenetur? Enim molestiae fugit, quisquam voluptas iure possimus odit eligendi excepturi placeat rem aperiam dolorem voluptate dignissimos doloremque fuga amet vero quasi accusamus libero consectetur quis. Commodi facere obcaecati est soluta ad totam illo provident itaque perspiciatis eaque explicabo laboriosam, excepturi odit quia, ex, necessitatibus facilis delectus ipsum voluptatem voluptatibus assumenda quidem molestiae. Velit doloribus optio doloremque id est ex eos earum distinctio! Illum mollitia quam vitae.</div>
             </div>
-            <div class="block-info">
-                    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quam. Itaque, obcaecati maxime tempora doloremque sequi, natus dolorem perferendis illo nostrum unde cupiditate rerum doloribus dicta repellendus ipsa odio repudiandae nisi fuga vitae tenetur? Enim molestiae fugit, quisquam voluptas iure possimus odit eligendi excepturi placeat rem aperiam dolorem voluptate dignissimos doloremque fuga amet vero quasi accusamus libero consectetur quis. Commodi facere obcaecati est soluta ad totam illo provident itaque perspiciatis eaque explicabo laboriosam, excepturi odit quia, ex, necessitatibus facilis delectus ipsum voluptatem voluptatibus assumenda quidem molestiae. Velit doloribus optio doloremque id est ex eos earum distinctio! Illum mollitia quam vitae.</div>
-                <div class="color" id="me">It's Me</div>
+            <div className="block-info">
+                    <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quam. Itaque, obcaecati maxime tempora doloremque sequi, natus dolorem perferendis illo nostrum unde cupiditate rerum doloribus dicta repellendus ipsa odio repudiandae nisi fuga vitae tenetur? Enim molestiae fugit, quisquam voluptas iure possimus odit eligendi excepturi placeat rem aperiam dolorem voluptate dignissimos doloremque fuga amet vero quasi accusamus libero consectetur quis. Commodi facere obcaecati est soluta ad totam illo provident itaque perspiciatis eaque explicabo laboriosam, excepturi odit quia, ex, necessitatibus facilis delectus ipsum voluptatem voluptatibus assumenda quidem molestiae. Velit doloribus optio doloremque id est ex eos earum distinctio! Illum mollitia quam vitae.</div>
+                <div className="color" id="me">It's Me</div>
             </div>
         </div>
       </div>
