51c484a6cdd04673b4f1c8ef4f9bb2324ab6a9db
Lena Ryan <alissadyiphil@gmail.com>
2018-12-04 18:45:56 +0300

    small typo fix

--- src/Footer.js
+++ src/Footer.js
@@ -91,7 +91,7 @@ class Footer extends Component {
             <FootInside>
                 <p>Contact me:</p>
                 <a href="mailto:enenanichutina@gmail.com">
-                    <span className="fcol1">enenanichutina</span>
+                    <span className="fcol1">elenanichutina</span>
                     <span className="fcol2">@</span>
                     <span className="fcol3">gmail</span>
                     <span className="fcol4">.com</span>
