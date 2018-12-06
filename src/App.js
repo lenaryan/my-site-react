3b4dccf37b10c0df47a6e9abb2a660c96caef264
Lena Ryan <alissadyiphil@gmail.com>
2018-12-06 12:23:46 +0300

    deleted stuff I don't need

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
-import Contact from './components/Contact';
-import About from './components/About';
-import Post from './components/Post';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
+        <div className="App grey darken-1 white-text">
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/:post_id" component={Post} />
           </Switch>
         </div>
       </BrowserRouter>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,11 @@ import {Link, NavLink, withRouter} from 'react-router-dom';
 
 const Navbar = (props) => {
     return (
-        <nav className="nav-wrapper red darken-3">
+        <nav className="nav-wrapper light-green accent-4">
             <div className="container">
-                <a className="brand-logo">Poke'Times</a>
+                <a className="brand-logo">MetalWebDev</a>
                 <ul className="right">
                     <li><Link to="/">Home</Link></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
