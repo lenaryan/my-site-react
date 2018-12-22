8e1cf4b96557249a454096e33a6823bc8d372da0
lenaryan <35894595+lenaryan@users.noreply.github.com>
2018-12-22 23:46:59 +0300

    changed the works page layout

--- src/components/pages/Works.js
+++ src/components/pages/Works.js
@@ -4,10 +4,8 @@ import M from 'materialize-css';
 class Works extends Component {
     componentDidMount() {
         document.addEventListener('DOMContentLoaded', function() {
-            var elems = document.querySelectorAll('.carousel');
-            var instances = M.Carousel.init(elems, {numVisible: 7});
-            var elems_box = document.querySelectorAll('.materialboxed');
-            var instances_box = M.Materialbox.init(elems_box);
+            var elems = document.querySelectorAll('.materialboxed');
+            var instances = M.Materialbox.init(elems);
         });
     }
 
@@ -15,14 +13,14 @@ class Works extends Component {
         return (
             <div className="container works">
                 <h1 className="center">My Works</h1>
-                <div className="carousel">
-                    <a href="#one!" className="carousel-item materialboxed"><img src={require("../../images/guitar.jpg")} alt="slide one"/></a>
-                    <a href="#two!" className="carousel-item materialboxed"><img src={require("../../images/guitar.jpg")} alt="slide two"/></a>
-                    <a href="#three!" className="carousel-item materialboxed"><img src={require("../../images/guitar.jpg")} alt="slide three"/></a>
-                    <a href="#four!" className="carousel-item materialboxed"><img src={require("../../images/guitar.jpg")} alt="slide four"/></a>
-                    <a href="#five!" className="carousel-item materialboxed"><img src={require("../../images/guitar.jpg")} alt="slide five"/></a>
-                    <a href="#six!" className="carousel-item materialboxed"><img src={require("../../images/guitar.jpg")} alt="slide six"/></a>
-                    <a href="#seven!" className="carousel-item materialboxed"><img src={require("../../images/guitar.jpg")} alt="slide seven"/></a>
+                <div className="works-pics">
+                    <img src={require("../../images/guitar.jpg")} alt="work one" className="materialboxed" />
+                    <img src={require("../../images/guitar.jpg")} alt="work two" className="materialboxed" />
+                    <img src={require("../../images/guitar.jpg")} alt="work three" className="materialboxed" />
+                    <img src={require("../../images/guitar.jpg")} alt="work four" className="materialboxed" />
+                    <img src={require("../../images/guitar.jpg")} alt="work five" className="materialboxed" />
+                    <img src={require("../../images/guitar.jpg")} alt="work six" className="materialboxed" />
+                    <img src={require("../../images/guitar.jpg")} alt="work seven" className="materialboxed" />
                 </div>
             </div>
         )
