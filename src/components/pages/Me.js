0779315fab69d1b06af520d57a79b101a701d735
Lena Ryan <alissadyiphil@gmail.com>
2018-12-25 16:38:36 +0300

    page about me

--- src/components/pages/Me.js
+++ src/components/pages/Me.js
@@ -4,6 +4,24 @@ const Me = () => {
     return (
         <div className="container me">
             <h1 className="center">Hey, I'm Lena!</h1>
+            <div className="text-me">
+                <img class="materialboxed right z-depth-3" alt="Lena Ryan" src={require('../../images/me.jpg')} />
+                <p>My full name is Elena Nichutina, Lena Ryan is a nickname for my art, music and everything at all.</p>
+                <p>I love frontend. I breathe music, especially metal music.</p>
+                <p>At the moment I'm working for a web studio as a web dev on different CMS in Apatity, Murmansk region, Russia.</p>
+                <p>I want to become a frontend React dev so I learn React in my spare time.</p>
+                <p>Also I:</p>
+                <ul>
+                    <li><i class="material-icons light-green-text text-accent-2 left">arrow_forward</i>create CSS-images on Codepen: codepen.io/lenaryan/</li>
+                    <li><i class="material-icons light-green-text text-accent-2 left">arrow_forward</i>can speak Polish (had been living in Warsaw for 15 months)</li>
+                    <li><i class="material-icons light-green-text text-accent-2 left">arrow_forward</i>squat 12x30kg (the number is growing)</li>
+                    <li><i class="material-icons light-green-text text-accent-2 left">arrow_forward</i>growl like a male death metal vocalist</li>
+                    <li><i class="material-icons light-green-text text-accent-2 left">arrow_forward</i>adore watching YouTube tutorials and conference talks</li>
+                    <li><i class="material-icons light-green-text text-accent-2 left">arrow_forward</i>got all my frontend knowledge on YouTube</li>
+                    <li><i class="material-icons light-green-text text-accent-2 left">arrow_forward</i>ride a mini-cruiser in summer and a snowboard in winter</li>
+                    <li><i class="material-icons light-green-text text-accent-2 left">arrow_forward</i>want a corgi puppy</li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/components/pages/Works.js
+++ src/components/pages/Works.js
@@ -15,35 +15,35 @@ class Works extends Component {
                 <h1 className="center">My Works</h1>
                 <p className="center">Hover over an image to look at details. Flexbox, RWD and scripts are not mentioned, as they are used by me everywhere and every time. In parentheses I wrote what I was responsible for. If you see just a CMS title, it means everything on a website was made by me. Except designing it, of course.</p>
                 <div className="works-pics">
-                    <img src={require("../../images/site-cancer.jpg")} alt="work one" className="materialboxed" 
+                    <img src={require("../../images/site-cancer.jpg")} alt="work one" className="materialboxed z-depth-3" 
                         title="zhivojrak.ru - Joomla" />
-                    <img src={require("../../images/site-estetik.jpg")} alt="work two" className="materialboxed" 
+                    <img src={require("../../images/site-estetik.jpg")} alt="work two" className="materialboxed z-depth-3" 
                         title="estetik51.ru - Joomla (RWD, changed main and some other pages)" />
-                    <img src={require("../../images/site-imkofe.jpg")} alt="work three" className="materialboxed"
+                    <img src={require("../../images/site-imkofe.jpg")} alt="work three" className="materialboxed z-depth-3"
                         title="imkofe.ru - Bitrix (pages, RWD), next devs changed some details and lost the cart icon" />
-                    <img src={require("../../images/site-glu.jpg")} alt="work four" className="materialboxed" 
+                    <img src={require("../../images/site-glu.jpg")} alt="work four" className="materialboxed z-depth-3" 
                         title="gl-u.ru - Joomla (RWD and some changes)" />
-                    <img src={require("../../images/site-wallet.jpg")} alt="work five" className="materialboxed"
+                    <img src={require("../../images/site-wallet.jpg")} alt="work five" className="materialboxed z-depth-3"
                         title="newwallet.ru - Joomla (RWD, some details and pages, ajax)" />
-                    <img src={require("../../images/site-trans.jpg")} alt="work six" className="materialboxed" 
+                    <img src={require("../../images/site-trans.jpg")} alt="work six" className="materialboxed z-depth-3" 
                         title="trans-art.ru - WP" />
-                    <img src={require("../../images/site-trans-del.jpg")} alt="work seven" className="materialboxed" 
+                    <img src={require("../../images/site-trans-del.jpg")} alt="work seven" className="materialboxed z-depth-3" 
                         title="trans-art.ru/delivery - WP" />
-                    <img src={require("../../images/site-mtseti.jpg")} alt="work eight" className="materialboxed"
+                    <img src={require("../../images/site-mtseti.jpg")} alt="work eight" className="materialboxed z-depth-3"
                         title="mtseti.ru - Joomla" />
-                    <img src={require("../../images/site-rezina.png")} alt="work nine" className="materialboxed"
+                    <img src={require("../../images/site-rezina.png")} alt="work nine" className="materialboxed z-depth-3"
                         title="росрезинатехника.рф (pk-rezinotehnika.ru) - Joomla" />
-                    <img src={require("../../images/site-tlset.jpg")} alt="work ten" className="materialboxed"
+                    <img src={require("../../images/site-tlset.jpg")} alt="work ten" className="materialboxed z-depth-3"
                         title="tlset.ru - Birtix, used CSS Grid (everything except horrible looking banners, sticked pages to the sidebar and repeating icons in the sidebar)" />
-                    <img src={require("../../images/site-sport.jpg")} alt="work eleven" className="materialboxed" 
+                    <img src={require("../../images/site-sport.jpg")} alt="work eleven" className="materialboxed z-depth-3" 
                         title="uxsports.ru - WP, used Parallax" />
-                    <img src={require("../../images/site-sila.jpg")} alt="work twelve" className="materialboxed" 
+                    <img src={require("../../images/site-sila.jpg")} alt="work twelve" className="materialboxed z-depth-3" 
                         title="sila-tishiny.ru - Bitrix" />
-                    <img src={require("../../images/site-tender.png")} alt="work thirteen" className="materialboxed"
+                    <img src={require("../../images/site-tender.png")} alt="work thirteen" className="materialboxed z-depth-3"
                         title="new.северсайт.рф - MODx" />
-                    <img src={require("../../images/site-reklama.png")} alt="work fourteen" className="materialboxed" 
+                    <img src={require("../../images/site-reklama.png")} alt="work fourteen" className="materialboxed z-depth-3" 
                         title="adv51.ru - WP (main page, services page)" />
-                    <img src={require("../../images/site-accent.jpg")} alt="work fifteen" className="materialboxed"
+                    <img src={require("../../images/site-accent.jpg")} alt="work fifteen" className="materialboxed z-depth-3"
                         title="accent.северсайт.рф - Bitrix" />
                 </div>
             </div>
