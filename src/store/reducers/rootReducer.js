7b11558dbe195b892e428661fafc544d36593bdc
lenaryan <35894595+lenaryan@users.noreply.github.com>
2019-01-08 20:28:27 +0300

    synching a blog component with Firebase collection

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import postReducer from './postReducer';
 import authReducer from './authReducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
     post: postReducer,
-    auth: authReducer
+    auth: authReducer,
+    firestore: firestoreReducer
 });
 
 export default rootReducer;
